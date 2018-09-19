afb73a51f05043b7ec81ff706da851cf3696e614
Nate Miller <natekmiller96@gmail.com>
2018-09-19 21:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ function distanceFromHqInBlocks(distance){
 
 function distanceFromHqInFeet(distance){
   let result;
-  
+  if (distance >= 42){
+  result = (distance - 42) * 264;
+  } else {
+    result = (42 - distance) * 264;
+  }
+  return result;
 }
